379cb0aa3d3598d52fc46c9d66522ba2b9b76c7f
jennykwok <dreamergirl535@Jenny-Kwoks-MacBook-Pro.local>
2014-12-03 18:53:47 -0800

    jjenny eats doodooo

---
+++ DivideTableViewController.swift
@@ -0,0 +1,133 @@
+//
+//  DivideTableViewController.swift
+//  CIS55FinalProject
+//
+//  Created by Jenny Kwok on 12/2/14.
+//  Copyright (c) 2014 David Phan. All rights reserved.
+//
+
+import UIKit
+
+class DivideTableViewController: UITableViewController {
+    
+    
+    @IBOutlet weak var BillTotalText: UITextField!
+    @IBOutlet weak var TipText: UITextField!
+    @IBOutlet weak var NumberPeopleText: UITextField!
+    @IBOutlet weak var BillPerPersonLabel: UILabel!
+    
+    
+    
+    func calculation(){
+        var billTotal = BillTotalText.text
+        var tip = TipText.text
+        var numPeople = NumberPeopleText.text
+        
+        
+        var billPer = ((billTotal as NSString).doubleValue * (1 + ((tip as NSString).doubleValue/100))) / ((numPeople as NSString).doubleValue)
+        
+        BillPerPersonLabel.text = NSString(format: "The bill per person is $%.2f.", billPer);
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func textFieldShouldReturn(textField: UITextField!) -> Bool // called when 'return' key pressed. return NO to ignore.
+    {
+        textField.resignFirstResponder()
+        return true;
+    }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent)
+    {
+        BillTotalText.resignFirstResponder()
+        TipText.resignFirstResponder()
+        NumberPeopleText.resignFirstResponder() 
+    }
+
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Potentially incomplete method implementation.
+        // Return the number of sections.
+        return 0
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        return 0
+    }
+
+    @IBAction func CalculationButton(sender: AnyObject) {
+        calculation()
+    }
+    /*
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as UITableViewCell
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
