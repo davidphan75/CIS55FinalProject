7cd69da7f7f30b65f15332fd5c7cc0380332da88
jennykwok <jennykwok95@yahoo.com>
2014-12-05 10:49:17 -0800

    Navigation Bar

--- FPMainDebtViewController.swift
+++ FPMainDebtViewController.swift
@@ -28,7 +28,21 @@ class FPMainDebtViewController: UIViewController {
     }
     
     
-    
+    override func viewDidAppear(animated: Bool) {
+        
+        var nav = self.navigationController?.navigationBar
+        
+        nav?.barStyle = UIBarStyle.Black
+        nav?.tintColor = UIColor.yellowColor()
+        
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 35, height: 35))
+        imageView.contentMode = .ScaleAspectFit
+        
+        let image = UIImage(named: "logo.png")
+        imageView.image = image
+        
+        navigationItem.titleView = imageView
+    }
     
     //Core Data
     
@@ -54,6 +68,7 @@ class FPMainDebtViewController: UIViewController {
         fetchRequest.sortDescriptors = [sortDescriptor]
         return fetchRequest
     }
+    
     
 
     
